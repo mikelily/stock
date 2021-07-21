eb317f54ec7080add1cb3bf71cd932bef67023e6
mike01_lin <mike01_lin@tti.tv>
2021-07-21 23:07:48 +0800

    Initial Commit

---
+++ stock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stock
+//
+//  Created by 林華淵 on 2021/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
